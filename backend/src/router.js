624116f4bc9275c6be652d9da758ea65d3819960
afernandesabreu <178678281+afernandesabreu@users.noreply.github.com>
2024-09-27 13:30:46 -0300

    adicionando rotas para serviços

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const prestadorController = require('./controllers/prestadorController');
+const servicoController = require('./controllers/servicoController');
 const router = express.Router();
 
 router.get('/prestador', prestadorController.getAll);
+router.post('/servicos', servicoController.createServico);
+router.put('/servicos/:id', servicoController.updateServico);
+
 
 
 module.exports = router;
