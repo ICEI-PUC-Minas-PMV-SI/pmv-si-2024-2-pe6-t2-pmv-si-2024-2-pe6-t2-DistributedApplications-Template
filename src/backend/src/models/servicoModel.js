390e02d726e20b0ed2445483250387c742bc1121
afernandesabreu <178678281+afernandesabreu@users.noreply.github.com>
2024-09-28 19:41:07 -0300

    efetuando correcao na escrita

--- src/backend/src/models/servicoModel.js
+++ src/backend/src/models/servicoModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const servicoSchema = new mongoose.Schema({
     descricao: { type: String, required: true },
-    preco: { type: Number, require: true },
+    preco: { type: Number, required: true },
     duracao: { type: Number, required: true }
 });
 
