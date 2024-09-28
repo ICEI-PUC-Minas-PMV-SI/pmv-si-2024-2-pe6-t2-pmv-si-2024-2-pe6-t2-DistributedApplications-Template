ed4723cec6a5c2ef54af07fe80117645be222d86
Higor Mendes <higormendes.261@gmail.com>
2024-09-28 14:51:32 -0300

    Atualizando repositorio

--- src/backend/src/db/conn.js
+++ src/backend/src/db/conn.js
@@ -6,7 +6,7 @@ async function main() {
 
         // Coloque sua connection string diretamente aqui para testar
         await mongoose.connect(
-            'mongodb+srv://tfarias:8IsH3wpHI0PqrA3J@cluster0.pgwnp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+            'mongodb+srv://thalit:teste123@cluster0.pgwnp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
         );
         console.log('Conectado ao banco');
     } catch (error) {
