49e017dc35659dabebdd5adf9b75670c39b96194
Estêvão Moura Rodrigues <102765456+estevaomouraa@users.noreply.github.com>
2024-09-29 20:15:43 -0300

    Update Client.js

--- src/classe-a-rest-api/models/Client.js
+++ src/classe-a-rest-api/models/Client.js
@@ -1,5 +1,10 @@
 export default class Client {
-  constructor(id) {
-    (this.id = id);
+  constructor(id, companyName, contactInfo, userId, contractUrl, planId) {
+    (this.id = id), 
+    (this.companyName = companyName), 
+    (this.contactInfo = contactInfo), 
+    (this.userId = userId),
+    (this.contractUrl = contractUrl),
+    (this.planId = planId);
   }
 }
