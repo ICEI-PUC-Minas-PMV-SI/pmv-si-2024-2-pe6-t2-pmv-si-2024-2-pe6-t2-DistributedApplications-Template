1430aa74e2d23e2098ddee1c08f968cdc4badb1e
Amanda Andrade <96007035+amandaJrU@users.noreply.github.com>
2024-09-29 20:29:35 -0300

    Add Camapaign

--- src/classe-a-rest-api/models/Campaign.js
+++ src/classe-a-rest-api/models/Campaign.js
@@ -1,5 +1,9 @@
 export default class Campaign {
-  constructor(id) {
-    (this.id = id);
+  constructor(id, clientId, title, budget, postCreativeId) {
+    (this.id = id), 
+    (this.clientId = clientId), 
+    (this.title = title), 
+    (this.budget = budget),
+    (this.postCreativeId = postCreativeId);
   }
 }
