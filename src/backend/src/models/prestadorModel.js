8118ad8ef8952e1184168fbbc8f8f109bdc6099d
Higor Mendes <higormendes.261@gmail.com>
2024-10-07 18:55:37 -0300

    Adicionando criptografia na senha ao cadastrar um prestador

--- src/backend/src/models/prestadorModel.js
+++ src/backend/src/models/prestadorModel.js
@@ -6,24 +6,33 @@ const prestadorSchema = new mongoose.Schema({
     nome: { type: String, required: true },
     cnpj: { type: String, required: true },
     telefone: { type: String, required: true },
-    endereco:{ type: String, required: true },
+    endereco: { type: String, required: true },
     password: { type: String, required: true }
-    
 });
 
+
+prestadorSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        const salt = await bcryptjs.genSalt();
+        this.password = await bcryptjs.hash(this.password, salt);
+    }
+    next();
+});
+
+
 const Prestador = mongoose.model('Prestador', prestadorSchema);
+
 
 const getAll = async () => {
     return await Prestador.find();
 };
 
+
 const create = async (data) => {
     const novoPrestador = new Prestador(data);
     return await novoPrestador.save();
-
-    const salt = bcryptjs.genSaltSync();
-    this.body.password = bcryptjs.hashSync(this.body.password, salt);
 };
+
 
 const update = async (id, data) => {
     return await Prestador.findByIdAndUpdate(id, data, { new: true });
