349d526ff4fb688c5ca66e3dec9a97e99412c741
afernandesabreu <178678281+afernandesabreu@users.noreply.github.com>
2024-10-25 19:06:34 -0300

    criando componente servico

---
+++ src/frontend/src/components/Servico.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CadastroServico from './CadastroServico';
+
+const Servico = () => {
+    return(
+        <>
+            <CadastroServico />
+        </>
+    );
+}
+
+export default Servico;
