ac78a9e58a0baf2ac31d07eddf352df796922355
ThalitaOF <tfarias@sga.pucminas.br>
2024-10-30 20:22:55 -0300

    Ajuste layout login

--- src/frontend/src/components/Login.js
+++ src/frontend/src/components/Login.js
@@ -16,92 +16,74 @@ function Login() {
     async function handleLogin(e) {
         e.preventDefault();
         try {
-            const response = await api.post('/login', {
-                email,
-                password
-            });
-
-            if (response.status === 200) { // O status de sucesso pode ser 200
-                navigate('/home'); // Redireciona para a página de inicial
+            const response = await api.post('/login', { email, password });
+            if (response.status === 200) {
+                navigate('/home');
             }
         } catch (error) {
             console.error("Erro no Login:", error);
             if (error.response) {
-                // Verifica o status da resposta
                 if (error.response.status === 401) {
-                    alert("Senha incorreta!"); // Mensagem específica para senha incorreta     
+                    alert("Senha incorreta!");
                 } else if (error.response.status === 404) {
-                    alert("Usuário não encontrado!");} 
-                else if (error.response.data && error.response.data.message) {
-                    alert(error.response.data.message); // Exibe a mensagem de erro do servidor
+                    alert("Usuário não encontrado!");
+                } else if (error.response.data && error.response.data.message) {
+                    alert(error.response.data.message);
                 } else {
                     alert("Erro ao tentar realizar o login.");
                 }
-            } 
+            }
         }
     }
 
-
     return (
+        <Container fluid className="d-flex justify-content-center min-vh-100">
+            <Row className="text-center w-75" xs={1} md={6}>
+            <Col md={6} lg={4} className="mx-auto mt-5">
+                   <Col lg={8} className="mx-auto">
+                   <Image className="rounded mx-auto d-block" src="./icon.png" style={{ width: '75%' }} />
+                   <figcaption class="figure-caption"style={{ color: '#F79824', fontSize: '50px', textAlign: 'center', fontFamily: 'Luckiest Guy' }}>
+                   Agenda <br /> Fácil</figcaption>
 
-
-        <Container className='w-50 p-1' controlId="container">
-            <Row className='row justify-content-md-center'>
-                <Col xs={1} md={6}>
-                    <Image className='rounded mx-auto d-block w-75 p-2' src="./icon.png" />
+                   </Col>
+                    
+                
+                
+                    <Form className="shadow p-4 rounded text-start mt-3" style={{ border: '2px solid #7E5A9B', color:'#7E5A9B' }} onSubmit={handleLogin}>
+                        <Form.Group className="mb-3" controlId="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control
+                                type="email"
+                                required
+                                style={{ border: '2px solid #7E5A9B', borderRadius: '8px' }}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="password">
+                            <Form.Label>Senha</Form.Label>
+                            <Form.Control
+                                type="password"
+                                required
+                                style={{ border: '2px solid #7E5A9B', borderRadius: '8px' }}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </Form.Group>
+                        <div className="d-flex justify-content-end">
+                            <Button
+                                type="submit"
+                                className="btn-sm mt-2"
+                                style={{
+                                    backgroundColor: '#F79824',
+                                    borderColor: '#F79824',
+                                    fontSize: '20px',
+                                    borderRadius: '8px',
+                                }}
+                            >
+                                Entrar
+                            </Button>
+                        </div>
+                    </Form>
                 </Col>
-                <h1 style={{ color: '#F79824', fontSize: '35px', textAlign: 'center', fontFamily: 'Luckiest Guy' }}>
-                    Agenda <br /> Fácil
-                </h1>
-                <Form xs={1} md={6} className='shadow-sm w-100'
-                    style={{
-                        borderRadius: '8px',
-                        border: '3px solid #7E5A9B',
-                        color: '#7E5A9B',
-                        fontSize: '24px',
-                        margin: '0 auto'
-                    }}
-                    onSubmit={handleLogin}
-                >
-                    <Form.Group className="mb-3 mt-3" controlId="email">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control
-                            style={{
-                                border: '3px solid #7E5A9B',
-                                borderRadius: '8px'
-                            }}
-                            type="email"
-                            required
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="password">
-                        <Form.Label>Senha</Form.Label>
-                        <Form.Control
-                            style={{
-                                border: '3px solid #7E5A9B',
-                                borderRadius: '8px'
-                            }}
-                            type="password"
-                            required 
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </Form.Group>
-                    <div className='position-relative mb-2'>
-                        <Button type='submit'
-                            style={{
-                                backgroundColor: '#F79824',
-                                borderRadius: '8px',
-                                border: '3px solid #F79824',
-                                fontSize: '24px',
-                                fontFamily: 'inherit',
-                                justifyItems: 'end'
-                            }}
-                        > 
-                            Entrar
-                        </Button>
-                    </div>
-                </Form>
             </Row>
         </Container>
     );
