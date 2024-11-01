b35982b5831346247063e82c535a22a979e26923
afernandesabreu <178678281+afernandesabreu@users.noreply.github.com>
2024-11-01 18:34:04 -0300

    removendo funcoes adicionando identificacao de prestador

--- src/backend/src/models/servicoModel.js
+++ src/backend/src/models/servicoModel.js
@@ -1,33 +1,12 @@
 const mongoose = require('mongoose');
 
 const servicoSchema = new mongoose.Schema({
-    descricao: { type: String, required: true },
-    preco: { type: Number, required: true },
-    duracao: { type: Number, required: true }
+  descricao: { type: String, required: true },
+  preco: { type: Number, required: true },
+  duracao: { type: Number, required: true },
+  prestadorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Prestador', required: true }
 });
 
 const Servico = mongoose.model('Servico', servicoSchema);
 
-const getAll = async () => {
-    return await Servico.find();
-};
-
-const create = async (data) => {
-    const novoServico = new Servico(data);
-    return await novoServico.save();
-};
-
-const update = async (id, data) => {
-    return await Servico.findByIdAndUpdate(id, data, { new: true });
-};
-
-const remove = async (id) => {
-    return await Servico.findByIdAndDelete(id);
-};
-
-module.exports = {
-    getAll,
-    create,
-    update,
-    remove
-};
+module.exports = Servico;
