ad54897acf8c3ab97d6f03ec4b7a0c51f71a057f
afernandesabreu <178678281+afernandesabreu@users.noreply.github.com>
2024-12-05 22:00:33 -0300

    simplificando código em servicoModel

--- src/backend/src/models/servicoModel.js
+++ src/backend/src/models/servicoModel.js
@@ -4,55 +4,30 @@ const servicoSchema = new mongoose.Schema({
   descricao: { type: String, required: true },
   preco: { type: Number, required: true, min: 0 },
   duracao: { type: Number, required: true, min: 0 },
-  prestadorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Prestador', required: true },
+ // prestadorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Prestador', required: true },
 });
 
 const Servico = mongoose.model('Servico', servicoSchema);
 
 const getAllServicos = async () => {
-  try{
-    return await Servico.find();
-  } catch (error) {
-    console.error('Erro ao buscar serviços:', error);
-    throw error;
-  }
+  return await Servico.find();
 };
 
 const addServico = async (data) => {
-  try{
-    const novoServico = new Servico(data);
-    return await novoServico.save();
-  } catch (error){
-    console.error('Erro ao adicionar serviço:', error);
-    throw error;
-  }
+  const novoServico = new Servico(data);
+  return await novoServico.save();
 };
 
 const findServicoById = async (id) => {
-  try {
-    return await Servico.findById(id);
-  } catch (error) {
-    console.error('Erro ao buscar serviço por ID:', error);
-    throw error;
-  }
+  return await Servico.findById(id);
 };
 
 const updateServico = async (id, data) => {
-  try {
-    return await Servico.findByIdAndUpdate(id, data, { new: true });
-  } catch (error) {
-    console.error('Erro ao atualizar serviço:', error);
-    throw error;
-  }
+  return await Servico.findByIdAndUpdate(id, data, { new: true });
 };
 
 const deleteServico = async (id) => {
-  try {
-    return await Servico.findByIdAndDelete(id);
-  } catch (error) {
-    console.error('Erro ao deletar serviço:', error);
-    throw error;
-  }
+  return await Servico.findByIdAndDelete(id);
 };
 
 module.exports = {
@@ -60,6 +35,5 @@ module.exports = {
   addServico,
   findServicoById,
   updateServico,
-  deleteServico,
-  Servico
+  deleteServico
 };
