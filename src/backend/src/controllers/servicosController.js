679b96073cd6fd484a645b837c5e1fe57ddb6fa3
afernandesabreu <178678281+afernandesabreu@users.noreply.github.com>
2024-12-05 22:00:03 -0300

    adicionando e modificando funcoes de servicoController

--- src/backend/src/controllers/servicosController.js
+++ src/backend/src/controllers/servicosController.js
@@ -1,81 +1,88 @@
-const { Servico } = require('../models/servicoModel.js');
+const servicoModel = require('../models/servicoModel.js');
 
-const getAllServicos = async (req, res) => {
+const createServico = async (req, res) => {
+  const { descricao, preco, duracao } = req.body;
+
+  if (!descricao || !preco || !duracao) {
+    return res.status(400).json({ error: 'Por favor, preencha todos os campos.' });
+  }
+
   try {
-    const servicos = await Servico.find({ prestadorId: req.prestador._id });
-    return res.status(200).json(servicos);
+    const novoServico = await servicoModel.addServico({
+      descricao, 
+      preco, 
+      duracao, 
+      //prestadorId: req.prestador._id 
+    });
+    res.status(201).json( {servico: novoServico, mensagem: 'Serviço criado com sucesso!' });
   } catch (error) {
-    return res.status(500).json({ error: error.message });
+    res.status(500).json({ mensagem:'Erro ao criar serviço.', erro: error.message });
   }
 };
 
-const createServico = async (req, res) => {
-  //console.log('createServico chamado');
+
+const getAllServicos = async (req, res) => {
   try {
-    const { descricao, preco, duracao } = req.body;
-    if (!descricao || !preco || !duracao) {
-      return res.status(400).json({ error: 'Todos os campos são obrigatórios' });
-    }
-    const novoServico = await Servico.create({ descricao, preco, duracao, prestadorId: req.prestador._id });
-    return res.status(201).json(novoServico);
+    const servicos = await servicoModel.getAllServicos();
+    res.json(servicos);
   } catch (error) {
-    console.error('Erro ao criar serviço:', error);
-    return res.status(500).json({ error:'Erro ao criar serviço.' });
+    res.status(500).json({ mensagem: 'Erro ao obter serviços.', erro: error.message });
+  }
+};
+
+
+const getServicoById = async (req, res) => {
+  try {
+    const servico = await servicoModel.findServicoById(req.params.id);
+    if (!servico) {
+      return res.status(404).json({ mensagem: 'Serviço não encontrado!' });
+    }
+    res.json(servico);
+  } catch (error) {
+    res.status(500).json({ mensagem: 'Erro ao obter serviço.', erro: error.message });
   }
 };
 
 
 const updateServico = async (req, res) => {
-  //console.log('updateServico chamado');
   try {
-    const { id } = req.params;
-    const { descricao, preco, duracao } = req.body;
-    if (!descricao || !preco || !duracao) {
-      return res.status(400).json({ error: 'Todos os campos são obrigatórios'});
+    const servico = await servicoModel.updateServico(req.params.id, req.body);
+    if (!descricao) {
+      return res.status(404).json({ mensagem: 'Serviço não encontrado!'});
     }
-    const servicoAtualizado = await Servico.findOneAndUpdate(
-      { _id: id, prestadorId: req.prestador._id },
-      { descricao, preco, duracao },
-      { new: true }
-    );
-    if (!servicoAtualizado) {
-      return res.status(404).json({ error: 'Serviço não encontrado' });
-    }
-    return res.status(200).json(servicoAtualizado);
+    res.json({ servico, mensagem: 'Serviço atualizado com sucesso!' });
   } catch (error) {
-    console.error('Erro ao atualizar serviço:', error);
-    return res.status(500).json({ error: 'Erro ao atualizar serviço.' });
+    res.status(500).json({ mensagem: 'Erro ao atualizar serviço.', erro: error.message });
   }
 };
+
 
 const deleteServico = async (req, res) => {
   try {
-    const { id } = req.params;
-    const servico = await Servico.findOneAndDelete({ _id: id, prestadorId: req.prestador._id });
-    if (!servico) {
-      return res.status(404).json({ error: 'Serviço não encontrado' });
+    const deleted = await servicoModel.deleteServico(req.params.id);
+    if (!deleted) {
+      return res.status(404).json({ mensagem: 'Serviço não encontrado' });
     }
-    return res.status(204).send();
+    res.json({ mensagem: 'Cliente deletado com sucesso!' });
   } catch (error) {
-    console.error('Erro ao deletar serviço:', error);
-    return res.status(500).json({ error: 'Erro ao deletar serviço.' });
+    res.status(500).json({ mensagem: 'Erro ao deletar serviço.', erro: error.mesage });
   }
 };
 
-const getAllServicosByPrestador = async (req, res) => {
-  try {
-    const servicos = await Servico.find({prestadorId: req.prestador._id });
-    return res.status(200).json(servicos);
-  } catch (error) {
-    console.error('Erro ao buscar serviços:', error);
-    return res.status(500).json({ error: 'Erro ao buscar serviços.' });
-  }
-};
+// const getAllServicosByPrestador = async (req, res) => {
+//   try {
+//     const servicos = await Servico.find({prestadorId: req.prestador._id });
+//     return res.status(200).json(servicos);
+//   } catch (error) {
+//     console.error('Erro ao buscar serviços:', error);
+//     return res.status(500).json({ error: 'Erro ao buscar serviços.' });
+//   }
+// };
 
 module.exports = {
-  getAllServicos,
   createServico,
+  getAllServicos,
+  getServicoById,
   updateServico,
-  deleteServico,
-  getAllServicosByPrestador
+  deleteServico
 };
