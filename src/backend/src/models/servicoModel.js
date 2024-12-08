ae0c954c768fcf007b440932571c15883acd8943
afernandesabreu <178678281+afernandesabreu@users.noreply.github.com>
2024-12-07 21:14:12 -0300

    removendo funcoes,  adicionando prestadorId ao esquema

--- src/backend/src/models/servicoModel.js
+++ src/backend/src/models/servicoModel.js
@@ -2,38 +2,11 @@ const mongoose = require('mongoose');
 
 const servicoSchema = new mongoose.Schema({
   descricao: { type: String, required: true },
-  preco: { type: Number, required: true, min: 0 },
-  duracao: { type: Number, required: true, min: 0 },
- // prestadorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Prestador', required: true },
+  preco: { type: Number, required: true },
+  duracao: { type: Number, required: true },
+  prestadorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Prestador', required: true }
 });
 
 const Servico = mongoose.model('Servico', servicoSchema);
 
-const getAllServicos = async () => {
-  return await Servico.find();
-};
-
-const addServico = async (data) => {
-  const novoServico = new Servico(data);
-  return await novoServico.save();
-};
-
-const findServicoById = async (id) => {
-  return await Servico.findById(id);
-};
-
-const updateServico = async (id, data) => {
-  return await Servico.findByIdAndUpdate(id, data, { new: true });
-};
-
-const deleteServico = async (id) => {
-  return await Servico.findByIdAndDelete(id);
-};
-
-module.exports = {
-  getAllServicos,
-  addServico,
-  findServicoById,
-  updateServico,
-  deleteServico
-};
+module.exports = Servico;
