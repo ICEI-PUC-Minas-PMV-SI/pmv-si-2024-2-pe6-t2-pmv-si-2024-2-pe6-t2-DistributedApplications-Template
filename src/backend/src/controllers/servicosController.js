7a9a6b1996b5fbba10a5838b2179f622b101fa31
afernandesabreu <178678281+afernandesabreu@users.noreply.github.com>
2024-12-07 21:59:58 -0300

    adicionando id prestador em funcoes

--- src/backend/src/controllers/servicosController.js
+++ src/backend/src/controllers/servicosController.js
@@ -1,88 +1,64 @@
-const servicoModel = require('../models/servicoModel.js');
-
-const createServico = async (req, res) => {
-  const { descricao, preco, duracao } = req.body;
-
-  if (!descricao || !preco || !duracao) {
-    return res.status(400).json({ error: 'Por favor, preencha todos os campos.' });
-  }
-
-  try {
-    const novoServico = await servicoModel.addServico({
-      descricao, 
-      preco, 
-      duracao, 
-      //prestadorId: req.prestador._id 
-    });
-    res.status(201).json( {servico: novoServico, mensagem: 'Serviço criado com sucesso!' });
-  } catch (error) {
-    res.status(500).json({ mensagem:'Erro ao criar serviço.', erro: error.message });
-  }
-};
-
+const Servico = require('../models/servicoModel.js');
 
 const getAllServicos = async (req, res) => {
   try {
-    const servicos = await servicoModel.getAllServicos();
-    res.json(servicos);
+    const servicos = await Servico.find({ prestadorId: req.prestador._id });
+    return res.status(200).json(servicos);
   } catch (error) {
-    res.status(500).json({ mensagem: 'Erro ao obter serviços.', erro: error.message });
+    return res.status(500).json({ error: error.message });
   }
 };
 
-
-const getServicoById = async (req, res) => {
+const createServico = async (req, res) => {
+  console.log('createServico chamado');
   try {
-    const servico = await servicoModel.findServicoById(req.params.id);
-    if (!servico) {
-      return res.status(404).json({ mensagem: 'Serviço não encontrado!' });
-    }
-    res.json(servico);
+    const { descricao, preco, duracao } = req.body;
+    const novoServico = await Servico.create({ descricao, preco, duracao, prestadorId: req.prestador._id });
+    return res.status(201).json(novoServico);
   } catch (error) {
-    res.status(500).json({ mensagem: 'Erro ao obter serviço.', erro: error.message });
+    return res.status(500).json({ error: error.message });
   }
 };
-
 
 const updateServico = async (req, res) => {
+  console.log('updateServico chamado');
   try {
-    const servico = await servicoModel.updateServico(req.params.id, req.body);
-    if (!descricao) {
-      return res.status(404).json({ mensagem: 'Serviço não encontrado!'});
-    }
-    res.json({ servico, mensagem: 'Serviço atualizado com sucesso!' });
+    const { id } = req.params;
+    const { descricao, preco, duracao } = req.body;
+    const servicoAtualizado = await Servico.findOneAndUpdate(
+      { _id: id, prestadorId: req.prestador._id },
+      { descricao, preco, duracao },
+      { new: true }
+    );
+    return res.status(200).json(servicoAtualizado);
   } catch (error) {
-    res.status(500).json({ mensagem: 'Erro ao atualizar serviço.', erro: error.message });
+    return res.status(500).json({ error: error.message });
   }
 };
-
 
 const deleteServico = async (req, res) => {
   try {
-    const deleted = await servicoModel.deleteServico(req.params.id);
-    if (!deleted) {
-      return res.status(404).json({ mensagem: 'Serviço não encontrado' });
-    }
-    res.json({ mensagem: 'Cliente deletado com sucesso!' });
+    const { id } = req.params;
+    await Servico.findOneAndDelete({ _id: id, prestadorId: req.prestador._id });
+    return res.status(204).send();
   } catch (error) {
-    res.status(500).json({ mensagem: 'Erro ao deletar serviço.', erro: error.mesage });
+    return res.status(500).json({ error: error.message });
   }
 };
 
-// const getAllServicosByPrestador = async (req, res) => {
-//   try {
-//     const servicos = await Servico.find({prestadorId: req.prestador._id });
-//     return res.status(200).json(servicos);
-//   } catch (error) {
-//     console.error('Erro ao buscar serviços:', error);
-//     return res.status(500).json({ error: 'Erro ao buscar serviços.' });
-//   }
-// };
+const getAllServicosByPrestador = async (req, res) => {
+  try {
+    const servicos = await Servico.find({prestadorId: req.prestador._id });
+    return res.status(200).json(servicos);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
 
 module.exports = {
-  createServico,
   getAllServicos,
-  getServicoById,
+  createServico,
   updateServico,
-  deleteServico
+  deleteServico,
+  getAllServicosByPrestador
 };
