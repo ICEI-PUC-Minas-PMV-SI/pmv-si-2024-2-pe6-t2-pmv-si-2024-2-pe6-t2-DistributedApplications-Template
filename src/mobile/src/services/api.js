50cdd25546bca5d84d7c3a5b6237ec545c35162b
afernandesabreu <178678281+afernandesabreu@users.noreply.github.com>
2024-12-08 00:12:36 -0300

    adicionando funcao que recebe token e criar interceptor

--- src/mobile/src/services/api.js
+++ src/mobile/src/services/api.js
@@ -1,17 +1,18 @@
 import axios from "axios";
-import { useAuth } from "../components/AuthContext";
 
 const api = axios.create({
     baseURL: 'http://192.168.5.107:3000' // Substitua pelo seu IP
 });
 
-api.interceptors.request.use((config) => {
-    const { userToken } = useAuth();
-    if (userToken) {
-      config.headers.Authorization = `Bearer ${userToken}`;
-    }
-    return config;
-  });
+export const createApiWithToken = (token) => {
+    api.interceptors.request.use((config) => {
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    });
+    return api;
+};
 
 
 export default api;
