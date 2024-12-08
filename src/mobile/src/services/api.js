17baa9cdf3c69237d0b4f70c8a21c2c9768c77e3
afernandesabreu <178678281+afernandesabreu@users.noreply.github.com>
2024-12-07 23:27:30 -0300

    adicionando informacoes de contexto (authContext) para acesso ao token

--- src/mobile/src/services/api.js
+++ src/mobile/src/services/api.js
@@ -1,7 +1,17 @@
 import axios from "axios";
+import { useAuth } from "../components/AuthContext";
 
 const api = axios.create({
-    baseURL: 'http://192.168.1.4:3000' // Substitua pelo seu IP
+    baseURL: 'http://192.168.5.107:3000' // Substitua pelo seu IP
 });
+
+api.interceptors.request.use((config) => {
+    const { userToken } = useAuth();
+    if (userToken) {
+      config.headers.Authorization = `Bearer ${userToken}`;
+    }
+    return config;
+  });
+
 
 export default api;
