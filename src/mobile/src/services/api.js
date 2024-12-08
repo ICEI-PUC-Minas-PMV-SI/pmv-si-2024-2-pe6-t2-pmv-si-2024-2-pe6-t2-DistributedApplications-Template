af6511947eb283b38b6113b6dbb3ee1c2ecc7aeb
afernandesabreu <alexandre.fernabreu@outlook.com>
2024-12-08 20:42:36 -0300

    adicionando ip qlqr

--- src/mobile/src/services/api.js
+++ src/mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://192.168.5.107:3000' // Substitua pelo seu IP
+    baseURL: 'http://192.168.1.4:3000' // Substitua pelo seu IP
 });
 
 export const createApiWithToken = (token) => {
